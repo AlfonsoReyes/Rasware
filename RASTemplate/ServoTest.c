d271e59ca13988bedc445c89109b4deb61221c0c
AlfonsoReyes <alfonsoreyesaguayo@gmail.com>
2015-10-22 15:34:09 -0500

    -Changed servo software to use motor.h files instead of servo.h files -Cleaned up some of the comments, wasted lots of spacex

--- RASTemplate/ServoTest.c
+++ RASTemplate/ServoTest.c
@@ -7,8 +7,8 @@
 #include <RASLib/inc/adc.h>
 
 static tADC *adc[4];
-static tServo *servo1;
-static tServo *servo2;
+static tMotor *motor1;
+static tMotor *motor2;
 static tBoolean initialized = false;
 
 // The 'main' function is the entry point of the program
@@ -18,8 +18,8 @@ if (!(initialized)){
 
     initialized = true;
 
-    servo1 = InitializeServo(PIN_B0);
-    servo2 = InitializeServo(PIN_B1);
+    motor1 = InitializeServoMotor(PIN_B0,false);
+    motor2 = InitializeServoMotor(PIN_B1,false);
     adc[0] = InitializeADC(PIN_D0);
     adc[1] = InitializeADC(PIN_D1);
     adc[2] = InitializeADC(PIN_D2);
@@ -40,65 +40,18 @@ if (!(initialized)){
         ADCRead(adc[2]),
         ADCRead(adc[3])
         );
-    
+
+//adc is Analog to Digital Conversion
 
     if(ADCRead(adc[0]) > 0.90){
-        SetServo(servo1, 0.55);
-        SetServo(servo2, 0.45);
+        SetMotor(motor1, 0.25);
+        SetMotor(motor2, -0.25);
     }
     else{
-        SetServo(servo1, 0.45);
-        SetServo(servo2, 0.55);;
+        SetMotor(motor1, -0.25);
+        SetMotor(motor2, 0.25);
     }
 
     }
-}    
-
-//    Printf(" w - forward \n" "s - reverse\n" "a- left \n" "d - right \n");
-      
-//    ch = Getc();
-//    Printf("%c\n", ch);
-//    while (1) {
-//        // Runtime code can go here
-//        switch(ch){
-//      case 'w':           //forward
-//            pos1 = 0.75f;
-//            pos2 = 0.25f;           
-//            break;
-//           
-//        case 's':           //reverse
-//            pos1 = 0.25f;
-//
-//            pos2 = 0.75f;
-//           break;
-//           
-//        case 'a':           //left
-//            pos1 = 0.75f;
-//            pos2 = 0.50;
-//            break;
-//        case 'd':           //right
-//           pos1 = 0.50;
-//            pos2 = 0.75f;
-//            break;
-//           
-//        case 'x':           //stpp
-//            pos1 = 0.5;
-//            pos2 = 0.5;
-//            break;
-//        case 'l':
-//            return 0;
-//        default:
-//            pos1 = pos1;
-//            pos2 = pos2;       
-//        }
-  
-      
-
-//    SetServo(servo1,pos1);
-//    SetServo(servo2,pos2);
-
-   // ch = Getc();
-   
-//    }
-
+}
 
